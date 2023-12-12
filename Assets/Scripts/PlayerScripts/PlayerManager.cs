4e8635701eb468779fee40896f8dbd5a8e4c4eb8
Duck <110453028+Creator112234578@users.noreply.github.com>
2023-12-12 12:54:00 +0300

    shotgun not done yet

---
+++ Assets/Scripts/PlayerScripts/PlayerManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static float playerHP;
+    public static bool isGameOver;
+    public TextMeshProUGUI playerHPText;
+    public GameObject bloodOverlay;
+    public Slider healthSlider;
+    public Slider easeSlider;
+    public float maxHealth;
+    public float lerpHealth;
+    void Start()
+    {
+        isGameOver = false;
+        playerHP = 100;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHPText.text = "" + playerHP;
+        if (isGameOver)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        if (healthSlider.value != playerHP)
+        {
+           healthSlider.value = playerHP;
+        }
+        if (healthSlider.value != easeSlider.value)
+        {
+            easeSlider.value = Mathf.Lerp(easeSlider.value, playerHP, lerpHealth);
+        }
+    }
+            
+
+    public IEnumerator Damage (int damageAmount)
+    {
+        bloodOverlay.SetActive(true);
+        playerHP -= damageAmount;
+        if (playerHP <= 0)
+            isGameOver = true;
+        yield return new WaitForSeconds(1f);
+        bloodOverlay.SetActive(false);
+    }
+}
--- Assets/Scripts/PlayerScripts/Shooting.cs
+++ Assets/Scripts/PlayerScripts/Shooting.cs
@@ -20,24 +20,60 @@ public class Shooting : MonoBehaviour
 
     public GameObject Object;
     public GameObject KaboomObject;
+    public GameObject ShotgunObject;
+
+
+    public Weapons weapon;
+
+    public enum Weapons
+    {
+         pistol,
+         shotgun
+    }
 
     private void Start()
     {
+         weapon = Weapons.pistol;
          DuckInHand.SetActive(true);
     }
     // Update is called once per frame
     private void Update()
     {
+        WeaponSwitcher();
         Debug.DrawRay(transform.position, transform.TransformDirection (Vector3.forward) * 1000, Color.green);
-        if (Physics.Raycast(transform.position, transform.TransformDirection (Vector3.forward), out hited1, 1000, layer) && Input.GetMouseButton(0) && readyToThrow == true)
-        {   
-            Cam.LookAt(hited1.point);
-            Shoot();
+        if (weapon == Weapons.pistol)
+        {
+
+            if (Physics.Raycast(transform.position, transform.TransformDirection (Vector3.forward), out hited1, 1000, layer) && Input.GetMouseButton(0) && readyToThrow == true)
+            {   
+                Cam.LookAt(hited1.point);
+                Shoot();
+            }
+            else if (Physics.Raycast(transform.position, transform.TransformDirection (Vector3.forward), out hited2, 1000, layer) && Input.GetMouseButton(1) && readyToThrow == true)
+            {   
+                Cam.LookAt(hited2.point);
+                Kaboom();
+            }
         }
-        else if (Physics.Raycast(transform.position, transform.TransformDirection (Vector3.forward), out hited2, 1000, layer) && Input.GetMouseButton(1) && readyToThrow == true)
-        {   
-            Cam.LookAt(hited2.point);
-            Kaboom();
+        else if (weapon == Weapons.shotgun)
+        {
+
+             if (Physics.Raycast(transform.position, transform.TransformDirection (Vector3.forward), out hited1, 1000, layer) && Input.GetMouseButton(0) && readyToThrow == true)
+             {    
+                 Cam.LookAt(hited1.point);
+                 Shotgun();
+             }
+        }
+    }
+    void WeaponSwitcher()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            weapon = Weapons.pistol;
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            weapon = Weapons.shotgun;
         }
     }
     void Shoot()
@@ -45,7 +81,7 @@ public class Shooting : MonoBehaviour
         readyToThrow = false;
 
         Instantiate(Object, Cam.position, Cam.rotation);
-        shootCD = 0.5f;
+        shootCD = 0.25f;
         Debug.Log("WorkingShot");
         DuckInHand.SetActive(false);
         Invoke(nameof(ResetShoot), shootCD);
@@ -55,6 +91,15 @@ public class Shooting : MonoBehaviour
         readyToThrow = false;
         Instantiate(KaboomObject, Cam.position, Cam.rotation);
         shootCD = 2.5f;
+        Debug.Log("WorkingShot");
+        DuckInHand.SetActive(false);
+        Invoke(nameof(ResetShoot), shootCD);
+    }
+    void Shotgun()
+    {   
+        readyToThrow = false;
+        Instantiate(ShotgunObject, Cam.position, Cam.rotation);
+        shootCD = 0.5f;
         Debug.Log("WorkingShot");
         DuckInHand.SetActive(false);
         Invoke(nameof(ResetShoot), shootCD);
